863546e204a1a57ba9b75f24ab9bfd5b38944501
max-zuma <maxwelzuma@gmail.com>
2023-02-16 12:41:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <stdio>
 /**
  * main - Entry
  * Return: Always 0
  */
-int main(void) 
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
