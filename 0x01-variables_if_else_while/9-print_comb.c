e51370d89a084f1eec3c869a69ec3b53c73294ee
max-zuma <maxwelzuma@gmail.com>
2023-02-20 01:01:07 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n <= 57; n++)
 	{
 
 		putchair(n);
-
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
 
 		putchar('\n');
