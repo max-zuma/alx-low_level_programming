6d72aeea3a8fecb85c5146b7dc0a938c20c2c95c
max-zuma <maxwelzuma@gmail.com>
2023-02-22 10:38:04 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,20 +5,16 @@
  * @n: the number
  * Return: value of the last digit
  */
+
 int print_last_digit(int n)
 {
-	int a;
-
-	if (n < 0)
-		n = -n;
-
-	a = n % 10;
-
-	if (a < 0)
-
-	a = -a;
-
-	putchar(a + '0');
-
-	return (a);
+	int r;
+	
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	
+	return (0);
 }
