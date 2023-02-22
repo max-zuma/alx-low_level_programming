3857c4233ea6ce392ed1af5e1f3a0becc6183e87
max-zuma <maxwelzuma@gmail.com>
2023-02-22 00:34:17 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - print the sign of a num
  * @n: the num to be checked for
- * Returns: 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
