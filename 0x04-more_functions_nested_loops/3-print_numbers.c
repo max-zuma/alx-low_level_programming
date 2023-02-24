8a3463f7cf7a03e0eea263b959edc7d0adf117a2
max-zuma <maxwelzuma@gmail.com>
2023-02-24 01:48:07 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 48; ch  < 58; ch++)
+	for (c  = '0'; c  <= '9'; c++)
 	{
-		_putchar(ch);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
